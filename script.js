e4df21095f92647a5f3d00e496f0e9ccaf56634f
kaustubh0201 <kaustubh1818@gmail.com>
2021-03-29 22:16:21 +0530

    carousel  basics done

--- script.js
+++ script.js
@@ -19,7 +19,24 @@ $(document).ready(function(){
     // owl carousel script
 
     $('.carousel').owlCarousel({
-        
+        margin: 20,
+        loop: true,
+        autoplayTimeout: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0:{
+                ite,s: 1,
+                nav: false
+            },
+            600:{
+                items: 2,
+                nav: false
+            },
+            1000:{
+                items: 3,
+                nav: false
+            }
+        }
     });
 
 });
